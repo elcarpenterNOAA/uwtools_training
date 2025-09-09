d4a6a95fa35434404eff87c82a3d09547d4091ca
Christina.Holt <Christina.Holt@noaa.gov>
2025-09-09 18:24:06 +0000

    Adding a forecast script that handles ncat

---
+++ scripts/run_forecast.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+config=$1
+cycle=$2
+
+module load nco
+
+set -e
+
+# Prepare run directory
+uw fv3 provisidioned_rundir --cycle $cycle -c $config --key-path cycled_forecast --batch
+
+rundir=$(echo "cycle: \!datetime $cycle" | uw config realize -i $config --output-format yaml
+--key-path cycled_forecast.fv3.rundir --update-format yaml)
+
+for fn in $rundir/*.nc ; do
+  ncatted -a checksum,,d,, $fn
+done
+ncatted -O -a source,global,c,c,'FV3GFS GAUSSIAN NETCDF FILE' fv_core.res.tile1.nc
+
+uw fv3 run --cycle $cycle -c $config --key-path cycled_forecast --batch
