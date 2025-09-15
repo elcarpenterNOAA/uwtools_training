4cc05b7cb0999c01becae118c3947b30559b3d0a
Christina.Holt <Christina.Holt@noaa.gov>
2025-09-15 23:39:55 +0000

    Updated DRY configs.

--- scripts/run_forecast.sh
+++ scripts/run_forecast.sh
@@ -6,17 +6,17 @@ cycle=$2
 
 module load nco
 
-set -e
+set -eux
 
 # Prepare run directory
-uw fv3 provisidioned_rundir --cycle $cycle -c $config --key-path cycled_forecast --batch
+uw fv3 files_copied --cycle $cycle -c $config --key-path cycled_forecast --batch
 
-rundir=$(echo "cycle: \!datetime $cycle" | uw config realize -i $config --output-format yaml
+rundir=$(echo 'cycle: !datetime '$cycle | uw config realize -i $config --output-format yaml \
 --key-path cycled_forecast.fv3.rundir --update-format yaml)
 
-for fn in $rundir/*.nc ; do
+for fn in $rundir/INPUT/*.nc ; do
   ncatted -a checksum,,d,, $fn
 done
-ncatted -O -a source,global,c,c,'FV3GFS GAUSSIAN NETCDF FILE' fv_core.res.tile1.nc
+ncatted -O -a source,global,c,c,'FV3GFS GAUSSIAN NETCDF FILE' $rundir/INPUT/fv_core.res.tile1.nc
 
 uw fv3 run --cycle $cycle -c $config --key-path cycled_forecast --batch
